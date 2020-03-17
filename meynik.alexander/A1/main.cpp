2b4aacd25318d9f6c4e52103112746198ec4618b
MeynikAlexander <alartwins2001@gmail.com>
2020-03-17 13:43:18 +0300

    Solving some troublesome issues

--- meynik.alexander/A1/main.cpp
+++ meynik.alexander/A1/main.cpp
@@ -8,7 +8,7 @@ int main()
     {
         point_t mover{ 1.5,2.9 };
         Shape* shape1 = new Rectangle(point_t{ 2.0, 3.0 }, 4.0, 9.0);
-        std::cout << "Shape pointer initialised by rectangle object:" << std::endl;
+        std::cout << "Shape pointer initialised by rectangle object:" << std::endl
                   << "Rectangle before moving:" << std::endl;
         shape1->printInfo(std::cout);
         shape1->move(mover);
@@ -20,7 +20,7 @@ int main()
         std::cout << "Rectangle area: " << shape1->getArea() << std::endl << std::endl;
         delete shape1;
         Rectangle rect(point_t{ 2.0, 3.0 }, 4.0, 9.0);
-        std::cout << "Object of rectangle class:" << std::endl;
+        std::cout << "Object of rectangle class:" << std::endl
                   << "Rectangle before moving:" << std::endl;
         rect.printInfo(std::cout);
         rect.move(mover);
@@ -31,19 +31,19 @@ int main()
         rect.printInfo(std::cout);
         std::cout << "Rectangle area: " << rect.getArea() << std::endl << std::endl;
         Shape* shape2 = new Circle(point_t{ 1.2, 2.1 }, 2.281336);
-        std::cout << "Shape pointer initialised by circle object" << std::endl;
+        std::cout << "Shape pointer initialised by circle object" << std::endl
                   << "Circle before moving:" << std::endl;
         shape2->printInfo(std::cout);
         shape2->move(mover);
         std::cout << "Circle after moving:" << std::endl;
         shape2->printInfo(std::cout);
         shape2->move(-mover.x, -mover.y);
-        std::cout << "Circle returned to it's initial position:"  std::endl;
+        std::cout << "Circle returned to it's initial position:" << std::endl;
         shape2->printInfo(std::cout);
         std::cout << "Circle area: " << shape2->getArea() << std::endl << std::endl;
         delete shape2;
         Circle circ(point_t{ 1.2, 2.1 }, 2.281336);
-        std::cout << "Object of circle class:" << std::endl;
+        std::cout << "Object of circle class:" << std::endl
                   << "Circle before moving:" << std::endl;
         circ.printInfo(std::cout);
         circ.move(mover);
@@ -56,7 +56,7 @@ int main()
     }
     catch (const std::invalid_argument&arg)
     {
-        std::cerr << arg.what << std::endl;
+        std::cerr << arg.what() << std::endl;
         return 1;
     }
     catch (...)
