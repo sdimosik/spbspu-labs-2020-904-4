f7b977c08f21901d621a61c8ab0dd888526992d7
MeynikAlexander <alartwins2001@gmail.com>
2020-03-17 12:25:15 +0300

    Hope this will solve problem with line ends

---
+++ meynik.alexander/A1/circle.hpp
@@ -0,0 +1,26 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+#include "shape.hpp"
+#include <ostream>
+#define M_PI       3.14159265358979323846
+
+class Circle : public Shape
+{
+public:
+    Circle(const point_t& pos, double radius);
+
+    double getArea() const override;
+
+    rectangle_t getFrameRect() const override;
+
+    void move(const point_t& position) override;
+
+    void move(const double x, const  double y) override;
+
+    void printInfo(std::ostream& out) const override;
+
+private:
+    point_t centre_;
+    double radius_;
+};
+#endif
