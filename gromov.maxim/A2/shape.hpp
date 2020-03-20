49f0fab74c56884951d1aa8d4357d7996f8fb9fd
Maxim-byte <maxim5gromov@gmail.com>
2020-03-20 08:12:25 +0000

    Revert "delete"
    This reverts commit b99f83a93fa1280623e6805b80260d2e5f5c0206.

---
+++ gromov.maxim/A2/shape.hpp
@@ -0,0 +1,20 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include"base-types.hpp"
+
+namespace gromov
+{
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(const double x, const double y) = 0;
+    virtual void move(const point_t& center) = 0;
+    virtual void scale(const double coefficient) = 0;
+  };
+}
+
+#endif
