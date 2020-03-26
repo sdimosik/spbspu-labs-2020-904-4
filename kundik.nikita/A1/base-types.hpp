4099c64ab58035c2effdf9ab48f4f130aec650ac
Никита Кундик <nikitoskundik495@gmail.com>
2020-03-26 21:30:33 +0000

    xsdf sad asf a vbnm

---
+++ kundik.nikita/A1/base-types.hpp
@@ -0,0 +1,15 @@
+#ifndef base_types_hpp
+#define base_types_hpp
+
+struct point_t 
+{
+  double x;
+  double y;
+};
+struct rectangle_t 
+{
+  double width;
+  double height;
+  point_t pos;
+};
+#endif
