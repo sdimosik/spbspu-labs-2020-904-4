c2a593908d93a6349c6c829999bee3a07b59effa
Dmitry Shabinsky <sdimosikvip@gmail.com>
2020-06-16 23:20:50 +0700

    fix content

--- shabinsky.dmitry/A4/test-main.cpp
+++ shabinsky.dmitry/A4/test-main.cpp
@@ -5,108 +5,73 @@
 #include <boost/test/floating_point_comparison.hpp>
 #include "rectangle.hpp"
 #include "circle.hpp"
+#include "composite-shape.hpp"
+#include "matrix.hpp"
 
 const double ACCURACY_DOUBLE = 0.0001;
 
-BOOST_AUTO_TEST_CASE(test_invalid_argument_rectangle)
+BOOST_AUTO_TEST_CASE(test_rotate_circle)
 {
-  BOOST_CHECK_THROW(shabinsky::Rectangle rect({75.0, 76.0}, -10.0, 13.0), std::invalid_argument);
-};
-
-BOOST_AUTO_TEST_CASE(test_invalid_argument_rectangle_scale)
-{
-  shabinsky::Rectangle rect({75.0, 76.0}, 10.0, 13.0);
-  BOOST_CHECK_THROW(rect.scale(-1.0), std::invalid_argument);
-};
-
-BOOST_AUTO_TEST_CASE(test_permanency_move_rectange)
-{
-  shabinsky::Rectangle rect({75.0, 76.0}, 10.0, 13.0);
-  const shabinsky::rectangle_t frameRect = rect.getFrameRect();
-  const double area = rect.getArea();
-  rect.move({10.0, 10.0});
-  BOOST_CHECK_CLOSE(frameRect.height, rect.getFrameRect().height, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(frameRect.width, rect.getFrameRect().width, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(area, rect.getArea(), ACCURACY_DOUBLE);
-  rect.move(10.0, 10.0);
-  BOOST_CHECK_CLOSE(frameRect.height, rect.getFrameRect().height, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(frameRect.width, rect.getFrameRect().width, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(area, rect.getArea(), ACCURACY_DOUBLE);
+  shabinsky::Circle circle(10, {0, 0});
+  shabinsky::rectangle_t rectangle = circle.getFrameRect();
+  circle.rotate(56);
+  BOOST_CHECK(circle.getFrameRect() == rectangle);
 }
 
-BOOST_AUTO_TEST_CASE(test_correct_move_rectangle)
+BOOST_AUTO_TEST_CASE(test_rotate_rectangle)
 {
-  shabinsky::Rectangle rect({75.0, 76.0}, 10.0, 13.0);
-  const double x = -85.0,
-    y = 10.0,
-    oldX = rect.getPosition().x,
-    oldY = rect.getPosition().y;
-  rect.move(x, y);
-  BOOST_CHECK_CLOSE(rect.getPosition().x, x + oldX, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(rect.getPosition().y, y + oldY, ACCURACY_DOUBLE);
-  rect.move({x, y});
-  BOOST_CHECK_CLOSE(rect.getPosition().x, x, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(rect.getPosition().y, y, ACCURACY_DOUBLE);
+  shabinsky::Rectangle rectangle({0, 0}, 10, 10);
+  shabinsky::rectangle_t rectangle1 = rectangle.getFrameRect();
+  rectangle.rotate(90);
+  BOOST_CHECK(rectangle.getFrameRect() == rectangle1);
 }
 
-BOOST_AUTO_TEST_CASE(test_scale_rectangle_area)
+BOOST_AUTO_TEST_CASE(test_rotate_composite_shape)
 {
-  shabinsky::Rectangle rect({75.0, 76.0}, 10.0, 13.0);
-  double w1 = rect.getWidth(), h1 = rect.getHeight();
-  const double area_rect = rect.getArea(),
-    scale = 1.5;
-  rect.scale(scale);
-  w1 = rect.getWidth(), h1 = rect.getHeight();
-  const double area_rect1 = rect.getArea();
-  BOOST_CHECK_CLOSE(area_rect * scale * scale, rect.getArea(), ACCURACY_DOUBLE);
+  shabinsky::CompositeShape compositeShape;
+  std::shared_ptr<shabinsky::Shape> rect1 = std::make_shared<shabinsky::Rectangle>(shabinsky::Rectangle({1, 1}, 2, 2));
+  std::shared_ptr<shabinsky::Shape> rect2 = std::make_shared<shabinsky::Rectangle>(shabinsky::Rectangle({11, 1}, 2, 2));
+  std::shared_ptr<shabinsky::Shape> rect3 = std::make_shared<shabinsky::Rectangle>(shabinsky::Rectangle({4, 5}, 2, 2));
+  compositeShape.addShape(rect1);
+  compositeShape.addShape(rect2);
+  compositeShape.addShape(rect3);
+  compositeShape.rotate(90);
+  shabinsky::rectangle_t rectangle1{6, 12, {6, 3}};
+  BOOST_CHECK(compositeShape.getFrameRect() == rectangle1);
 }
 
-BOOST_AUTO_TEST_CASE(test_invalid_argument_circle)
+BOOST_AUTO_TEST_CASE(test_invalid_index_matrix)
 {
-  BOOST_CHECK_THROW(shabinsky::Circle circle(-13.0, {75.0, 76.0}), std::invalid_argument);
-};
-
-BOOST_AUTO_TEST_CASE(test_invalid_argument_circle_scale)
-{
-  shabinsky::Circle circle(13.0, {75.0, 76.0});
-  BOOST_CHECK_THROW(circle.scale(-1.0), std::invalid_argument);
-};
-
-BOOST_AUTO_TEST_CASE(test_permanency_move_circle)
-{
-  shabinsky::Circle circle(13.0, {75.0, 76.0});
-  const shabinsky::rectangle_t frameRect = circle.getFrameRect();
-  const double area = circle.getArea();
-  circle.move({10.0, 10.0});
-  BOOST_CHECK_CLOSE(frameRect.height, circle.getFrameRect().height, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(frameRect.width, circle.getFrameRect().width, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(area, circle.getArea(), ACCURACY_DOUBLE);
-  circle.move(10, 10);
-  BOOST_CHECK_CLOSE(frameRect.height, circle.getFrameRect().height, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(frameRect.width, circle.getFrameRect().width, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(area, circle.getArea(), ACCURACY_DOUBLE);
+  shabinsky::Matrix matrix;
+  BOOST_CHECK_THROW(matrix[0], std::out_of_range);
 }
 
-BOOST_AUTO_TEST_CASE(test_correct_move_circle)
+BOOST_AUTO_TEST_CASE(test_invalid_add_matrix)
 {
-  shabinsky::Circle circle(13.0, {75.0, 76.0});
-  const double x = -85.0,
-    y = 10.0,
-    oldX = circle.getPosition().x,
-    oldY = circle.getPosition().y;
-  circle.move(x, y);
-  BOOST_CHECK_CLOSE(circle.getPosition().x, x + oldX, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(circle.getPosition().y, y + oldY, ACCURACY_DOUBLE);
-  circle.move({x, y});
-  BOOST_CHECK_CLOSE(circle.getPosition().x, x, ACCURACY_DOUBLE);
-  BOOST_CHECK_CLOSE(circle.getPosition().y, y, ACCURACY_DOUBLE);
+  shabinsky::Matrix matrix;
+  shabinsky::Matrix::shapePtr shape = nullptr;
+  BOOST_CHECK_THROW(matrix.add(shape), std::invalid_argument);
 }
 
-BOOST_AUTO_TEST_CASE(test_scale_circle_area)
+BOOST_AUTO_TEST_CASE(test_invalid_index_removed_shape)
 {
-  shabinsky::Circle circle(13.0, {75.0, 76.0});
-  const double area_circle = circle.getArea(),
-    scale = 5.0;
-  circle.scale(scale);
-  BOOST_CHECK_CLOSE(area_circle * scale * scale, circle.getArea(), ACCURACY_DOUBLE);
+  shabinsky::Matrix matrix;
+  BOOST_CHECK_THROW(matrix.remove(0, 0), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE(test_add_and_remove_shape)
+{
+  shabinsky::Rectangle rectangle({0, 0}, 10, 10);
+  shabinsky::Circle circle(10, {0, 0});
+  shabinsky::Matrix::shapePtr shape1 = std::make_shared<shabinsky::Rectangle>(rectangle);
+  shabinsky::Matrix::shapePtr shape2 = std::make_shared<shabinsky::Circle>(circle);
+  shabinsky::Matrix matrix;
+  matrix.add(shape1);
+  matrix.add(shape2);
+  BOOST_CHECK(matrix[0][0] == shape1);
+  BOOST_CHECK(matrix[1][0] == shape2);
+  BOOST_CHECK(matrix.getRows() == 2);
+  BOOST_CHECK(matrix.getColumns() == 1);
+  matrix.remove(1, 0);
+  BOOST_CHECK(matrix[1][0] == nullptr);
 }
