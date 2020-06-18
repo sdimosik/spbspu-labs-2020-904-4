36534cc210796046064779342d3eb78e45ee49ee
Marina Biryukova <m.i.biryukova@gmail.com>
2020-06-18 10:43:45 +0000

    Созданы классы геометрических фигур.
    Реализованы классы Rectangle и Circle. Демонстрация в Main.cpp также включает полиморфное применение классов.

--- biryukova.marina/A4/main.cpp
+++ biryukova.marina/A4/main.cpp
@@ -8,9 +8,9 @@ int main()
 {
   try
   {
-    biryukova::Circle circle1(biryukova::point_t{ 1.8,4.2 }, 8.1);
-    biryukova::Circle circle2(biryukova::point_t{ 3.8,7.0 }, 1.4);
-    biryukova::Shape* shapes[2] = { &circle1, &circle2 };
+    biryukova::Circle circle1(biryukova::point_t{1.8,4.2}, 8.1);
+    biryukova::Circle circle2(biryukova::point_t{3.8,7.0}, 1.4);
+    biryukova::Shape* shapes[2] = {&circle1, &circle2};
     for (int i = 0; i < 2; i++)
     {
       std::cout << "Starting position of shape " << i + 1 << ":\n";
@@ -27,8 +27,8 @@ int main()
       std::cout << "Position after scaling with coefficient 3.1 of shape " << i + 1 << ":\n";
       shapes[i]->print();
     }
-    biryukova::Rectangle rectangle1(biryukova::point_t{ 3.0, 7.1 }, 9.4, 2.4);
-    biryukova::Rectangle rectangle2(biryukova::point_t{ 6.9, 5.2 }, 9.0, 10.5);
+    biryukova::Rectangle rectangle1(biryukova::point_t{3.0, 7.1}, 9.4, 2.4);
+    biryukova::Rectangle rectangle2(biryukova::point_t{6.9, 5.2}, 9.0, 10.5);
     shapes[0] = &rectangle1;
     shapes[1] = &rectangle2;
     for (int i = 0; i < 2; i++)
