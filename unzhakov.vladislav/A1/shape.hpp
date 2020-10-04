fd1e9b7d90dcbefd2a0fa306850e4f5445613176
Влад Унжаков <vladun25@gmail.com>
2020-10-04 17:36:03 +0000

    A1

---
+++ unzhakov.vladislav/A1/shape.hpp
@@ -0,0 +1,26 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "base-types.hpp"
+
+class Shape
+{
+public:
+  virtual ~Shape() = default;
+
+  virtual rectangle_t getFrameRect() const = 0; 
+
+  virtual double getArea() const = 0;
+
+  virtual void move(double x, double y) = 0;
+
+  virtual void move(const point_t& center) = 0;
+
+  virtual void printData() const = 0;
+
+  virtual void printFrameRectangle() const = 0;
+
+  virtual void printArea() const = 0;
+};
+
+#endif
