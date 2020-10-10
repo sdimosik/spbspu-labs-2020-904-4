175888e1aab3c9d06283c7d1d56cfad9d61ef77d
Kirill Zabelin <zabelinkirillv@gmail.com>
2020-10-10 22:02:51 +0300

    Revert "Merge branch 'master' into 'master'"
    This reverts commit 7008a44d61ca15c3090868e896dfae1bc806b549, reversing
    changes made to 5cd3a923132085ef2b823a6b876a4a8d38ae4232.

--- meshcheryakov.grigory/B1/tasks.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SPBSPU_LABS_2020_904_4_TASKS_HPP
-#define SPBSPU_LABS_2020_904_4_TASKS_HPP
-
-void task1(const char *);
-void task2(const char *);
-void task3();
-void task4(const char *, const int);
-
-#endif
