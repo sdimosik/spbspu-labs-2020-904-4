dfa2cad1ad7eb897f9ad36f22345a4c25e4a6783
Marina Biryukova <m.i.biryukova@gmail.com>
2020-11-09 14:01:58 +0000

    Созданы классы геометрических фигур.
    Реализованы классы Rectangle и Circle. Демонстрация в Main.cpp также включает полиморфное применение классов.

---
+++ biryukova.marina/B4/algorithm.hpp
@@ -0,0 +1,18 @@
+#ifndef ALGORITHM_HPP
+#define ALGORITHM_HPP
+#include <string>
+
+const int COMMAS_NUMBER = 2;
+const int MAX_KEY = 5;
+const int MIN_KEY = -5;
+
+void algorithm();
+
+struct DataStruct
+{
+  int key1;
+  int key2;
+  std::string str;
+};
+
+#endif
