b402607c09566c5be63a764556eedcef30d6167b
Mikhail Khilchenko <mihailkhilchenko@gmail.com>
2020-11-09 14:23:10 +0000

    Khilchenko Mkhail A1
    Copy replaced by link transfer. Added output of invalid arguments

---
+++ khilchenko.mikhail/B3/task2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+#include "factorial.hpp"
+
+void task2()
+{
+  Factorial factorial;
+  std::copy(factorial.begin(), factorial.end(), std::ostream_iterator<size_t>(std::cout, " "));
+  std::cout << '\n';
+  std::reverse_copy(factorial.begin(), factorial.end(), std::ostream_iterator<size_t>(std::cout, " "));
+  std::cout << '\n';
+}
